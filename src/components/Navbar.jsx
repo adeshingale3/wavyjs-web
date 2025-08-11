e1765cc1d1d1304e7ddb4b327419ec433ec497a0
adeshingale3 <adeshingale600@gmail.com>
2025-08-11 23:59:54 +0530

    ui fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import React, { useEffect } from 'react'
+import { FaGithub, FaXTwitter } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 
@@ -20,7 +21,7 @@ const Navbar = () => {
 
   return (
     <>
-      <ToastContainer />
+      <ToastContainer autoClose={5000} closeOnClick={true}/>
       <div className='nav absolute z-50 w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%] h-[45px] sm:h-[50px] md:h-[60px] flex flex-row items-center justify-center gap-6 sm:gap-8 md:gap-10 lg:gap-14 top-4 sm:top-6 md:top-10'>
 
         {/* Left menu */}
@@ -47,9 +48,9 @@ const Navbar = () => {
 
 
         {/* Right menu */}
-        <div className='bg-black/30 backdrop-blur-lg w-[40%] sm:w-[30%] h-full rounded-full shadow-inner shadow-white/20 flex flex-row items-center justify-between px-4 sm:px-5 md:px-7 gap-1 sm:gap-2'>
-          <Link onClick={notify} className='word text-white text-[10px] sm:text-xs md:text-sm' to="">Visit X</Link>
-          <Link onClick={notify} className='word text-white text-[10px] sm:text-xs md:text-sm' to="">GitHub</Link>
+        <div className='bg-black/30 backdrop-blur-lg w-[40%] sm:w-[30%] h-full rounded-full shadow-inner shadow-white/20 flex flex-row items-center justify-between px-6 sm:px-7 md:px-9 gap-1 sm:gap-2'>
+          <Link onClick={notify} className='word text-white text-[10px] sm:text-xs md:text-sm' to=""><FaXTwitter size={20}/></Link>
+          <Link onClick={notify} className='word text-white text-[10px] sm:text-xs md:text-sm' to=""><FaGithub size={20}/></Link>
         </div>
       </div>
 
--- src/screens/Hero.jsx
+++ src/screens/Hero.jsx
@@ -1,13 +1,14 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
-import React, { useEffect} from 'react'
+import React, { useEffect } from 'react'
 import SplitType from 'split-type'
 import Navbar from '../components/Navbar'
 import { useNavigate } from 'react-router-dom'
 import { toast, ToastContainer } from 'react-toastify'
+import { FaXTwitter } from 'react-icons/fa6'
 const Hero = () => {
     const navigate = useNavigate();
-  const notify = () => toast("Coming Soon !");
+    const notify = () => toast("Coming Soon !");
     useEffect(() => {
         const split = new SplitType(".title", {
             types: "lines",
@@ -23,9 +24,9 @@ const Hero = () => {
             stagger: 0.2,
         });
 
-        
 
-    
+
+
     }, []);
     useGSAP(() => {
         const screenWidth = window.innerWidth;
@@ -69,69 +70,76 @@ const Hero = () => {
 
     return (
         <>
-        
+
             <div className='h-screen w-screen flex justify-center'>
-                
-            <Navbar />
-            
-            <div className='font-Poppins h-screen w-screen bg-black flex items-center justify-center relative overflow-hidden'>
-            {/* LT Line */}
-            <div className='absolute h-[0.2%] w-[25%] bg-gradient-to-r from-black to-gray-500 top-50 left-0 rounded-full flex items-center'>
-                <h3 className='small-title absolute text-gray-400 text-[10px] ml-20 font-bold'>BUTTONS</h3>
-            </div>
-            {/* LT Circle */}
-            <div className='l-circle absolute h-2 w-2 bg-gray-400 top-49 left-0 rounded-full'></div>
-            {/* ---------------------------------------------------------- */}
-            {/* LB Line */}
-            <div className='absolute h-[0.2%] w-[25%] bg-gradient-to-r from-black to-gray-500 bottom-50 left-0 rounded-full flex items-center'>
-                <h3 className='small-title absolute text-gray-400 text-[10px] ml-20 font-bold'>CURSOR</h3>
-            </div>
-            {/* LB Circle */}
-            <div className='l-circle absolute h-2 w-2 bg-gray-400 bottom-49 left-0 rounded-full'></div>
-            {/* ----------------------------------------------------------- */}
-            {/* RT Line */}
-            <div className='absolute h-[0.2%] w-[25%] bg-gradient-to-l from-black to-gray-500 top-50 right-0 rounded-full flex items-center'>
-                <h3 className='small-title absolute text-gray-400 text-[10px] right-0 mr-20 font-bold'>CARDS</h3>
-            </div>
-            {/* RT Circle */}
-            <div className='r-circle absolute h-2 w-2 bg-gray-400 top-49 right-0 rounded-full'></div>
-            {/* ------------------------------------------------------------------- */}
-            {/* RB Line */}
-            <div className='absolute h-[0.2%] w-[25%] bg-gradient-to-l from-black to-gray-500 bottom-50 right-0 rounded-full flex items-center'>
-                <h3 className='small-titles absolute text-gray-400 text-[10px] right-0 mr-20 font-bold'>NAVBARS</h3>
-            </div>
-            {/* RB Circle */}
-            <div className='r-circle absolute h-2 w-2 bg-gray-400 bottom-49 right-0 rounded-full'></div>
-            {/* ------------------------------------------------------------------- */}
-            <div className='title w-[90%] sm:w-[80%] md:w-[60%] lg:w-[50%] xl:w-[40%] h-[50%] flex flex-col items-center justify-center text-center'>
-                <h3 className='text-[10px] sm:text-[12px] md:text-[14px] lg:text-[16px] mb-[10px] sm:mb-[12px] md:mb-[14px] lg:mb-[16px]  text-white font-semibold text-center bg-white/20 backdrop-blur-lg rounded-full py-1 px-4 shadow-inner shadow-white/20'>
-                    The foundation of your Design System
-                </h3>
-                <h1 className=' text-[40px] sm:text-[60px] md:text-[80px] lg:text-[100px] xl:text-[130px] font-bold text-white text-center leading-none'>
-                    WAVY JS
-                </h1>
-                <h2 className='text-[10px] sm:text-[12px] md:text-[14px] lg:text-[16px] text-center text-white mt-4'>
-                    A set of beautifully designed components that you can customize, extend, and build on. Start here then make it your own. Open Source. Open Code.
-                </h2>
-            </div>
 
-            <div className='buttons absolute bottom-30 w-[30vw] h-8 sm:h-10 md:h-12 lg:h-14 flex flex-row items-center justify-between gap-2 sm:gap-4 text-white z-9'>
-                <button onClick={notify} className='h-full w-[48%] bg-black/80 rounded-full backdrop-blur-lg text-[10px] sm:text-sm md:text-base lg:text-lg shadow-inner shadow-white/20 cursor-pointer'>
-                    Components
-                </button>
-                <button onClick={notify} className='h-full w-[40%] bg-black/80 rounded-full backdrop-blur-lg text-[10px] sm:text-sm md:text-base lg:text-lg shadow-inner shadow-white/20 cursor-pointer'>
-                    Visit X
-                </button>
+                <Navbar />
+
+                <div className='font-Poppins h-screen w-screen bg-black flex items-center justify-center relative overflow-hidden'>
+                    {/* LT Line */}
+                    <div className='absolute h-[0.2%] w-[25%] bg-gradient-to-r from-black to-gray-500 top-50 left-0 rounded-full flex items-center'>
+                        <h3 className='small-title absolute text-gray-400 text-[10px] ml-20 font-bold'>BUTTONS</h3>
+                    </div>
+                    {/* LT Circle */}
+                    <div className='l-circle absolute h-2 w-2 bg-gray-400 top-49 left-0 rounded-full'></div>
+                    {/* ---------------------------------------------------------- */}
+                    {/* LB Line */}
+                    <div className='absolute h-[0.2%] w-[25%] bg-gradient-to-r from-black to-gray-500 bottom-50 left-0 rounded-full flex items-center'>
+                        <h3 className='small-title absolute text-gray-400 text-[10px] ml-20 font-bold'>CURSOR</h3>
+                    </div>
+                    {/* LB Circle */}
+                    <div className='l-circle absolute h-2 w-2 bg-gray-400 bottom-49 left-0 rounded-full'></div>
+                    {/* ----------------------------------------------------------- */}
+                    {/* RT Line */}
+                    <div className='absolute h-[0.2%] w-[25%] bg-gradient-to-l from-black to-gray-500 top-50 right-0 rounded-full flex items-center'>
+                        <h3 className='small-title absolute text-gray-400 text-[10px] right-0 mr-20 font-bold'>CARDS</h3>
+                    </div>
+                    {/* RT Circle */}
+                    <div className='r-circle absolute h-2 w-2 bg-gray-400 top-49 right-0 rounded-full'></div>
+                    {/* ------------------------------------------------------------------- */}
+                    {/* RB Line */}
+                    <div className='absolute h-[0.2%] w-[25%] bg-gradient-to-l from-black to-gray-500 bottom-50 right-0 rounded-full flex items-center'>
+                        <h3 className='small-titles absolute text-gray-400 text-[10px] right-0 mr-20 font-bold'>NAVBARS</h3>
+                    </div>
+                    {/* RB Circle */}
+                    <div className='r-circle absolute h-2 w-2 bg-gray-400 bottom-49 right-0 rounded-full'></div>
+                    {/* ------------------------------------------------------------------- */}
+                    <div className='title w-[90%] sm:w-[80%] md:w-[60%] lg:w-[50%] xl:w-[40%] h-[50%] flex flex-col items-center justify-center text-center'>
+                        <h3 className='text-[10px] sm:text-[12px] md:text-[14px] lg:text-[16px] mb-[10px] sm:mb-[12px] md:mb-[14px] lg:mb-[16px]  text-white font-semibold text-center bg-white/20 backdrop-blur-lg rounded-full py-1 px-4 shadow-inner shadow-white/20'>
+                            The foundation of your Design System
+                        </h3>
+                        <h1 className=' text-[40px] sm:text-[60px] md:text-[80px] lg:text-[100px] xl:text-[130px] font-bold text-white text-center leading-none'>
+                            WAVY JS
+                        </h1>
+                        <h2 className='text-[10px] sm:text-[12px] md:text-[14px] lg:text-[16px] text-center text-white mt-4'>
+                            A set of beautifully designed components that you can customize, extend, and build on. Start here then make it your own. Open Source. Open Code.
+                        </h2>
+                    </div>
+
+                    <div className='buttons absolute bottom-20 w-[30vw] h-8 sm:h-10 md:h-12 lg:h-14 flex flex-row items-center justify-between gap-2 sm:gap-4 text-white z-9'>
+                        <button
+                            onClick={notify}
+                            className="h-10 sm:h-12 px-4 flex-1 sm:flex-none sm:w-[48%] bg-black/80 rounded-full backdrop-blur-lg text-[10px] sm:text-sm md:text-base lg:text-lg shadow-inner shadow-white/20 cursor-pointer"
+                        >
+                            Components
+                        </button>
+
+                        <button
+                            onClick={notify}
+                            className="h-10 sm:h-12 px-4 flex justify-center items-center sm:w-[40%] bg-black/80 rounded-full backdrop-blur-lg text-[10px] sm:text-sm md:text-base lg:text-lg shadow-inner shadow-white/20 cursor-pointer"
+                        >
+                            <FaXTwitter />
+                        </button>
+                    </div>
+                    <ToastContainer />
+
+
+
+
+
+
+                </div>
             </div>
-            <ToastContainer />
-
-           
-
-            
-
-
-        </div>
-        </div>
         </>
 
     )
